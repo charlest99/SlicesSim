5ebc1151d32f98c66b24713c758744daff637c67
Charles Tananbaum <charlestananbaum@gmail.com>
2018-10-29 22:25:05 -0400

    Update slicesSim.py

--- slicesSim.py
+++ slicesSim.py
@@ -155,24 +155,103 @@ def fillSum():
                         boards[it_o][c] = 0
     return pts
 
+def fillTotSum(): #checks to fill the circle with the most adjacent points
+    pieces = [0,1,2,3,4,5]
+    double_pieces = [[0,1], [1,2], [2,3], [3,4], [4,5], [5,0]]
+    boards = [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]]
+    pts = 0
+    x = True
+    while(x):
+        piece = 0
+        ran = randint(0, 5)
+        z = [[(sum(boards[0]) + sum(boards[1]) + sum(boards[3])),0],[(sum(boards[0]) + sum(boards[1]) + sum(boards[2])),1],[(sum(boards[2]) + sum(boards[1]) + sum(boards[3])),2],[(sum(boards[3]) + sum(boards[2]) + sum(boards[0])), 3]]
+        z.sort(key = lambda x: x[0], reverse=True)
+        if (ran == 0):
+            piece = double_pieces[ran]
+            a = piece[0]
+            b = piece[1]
+            if(boards[z[0][1]][a] == 0 & boards[z[0][1]][b] == 0):
+                boards[z[0][1]][a] = 1
+                boards[z[0][1]][b] = 1
+                pts+=2
+            elif(boards[z[1][1]][a] == 0 & boards[z[1][1]][b] == 0):
+                boards[z[1][1]][a] = 1
+                boards[z[1][1]][b] = 1
+                pts+=2
+            elif(boards[z[2][1]][a] == 0 & boards[z[2][1]][b] == 0):
+                boards[z[2][1]][a] = 1
+                boards[z[2][1]][b] = 1
+                pts+=2
+            elif(boards[z[3][1]][a] == 0 & boards[z[3][1]][b] == 0):
+                boards[z[3][1]][a] = 1
+                boards[z[3][1]][b] = 1
+                pts+=2
+            else:
+                x = False
+        else:
+            a = pieces[ran]
+            if(boards[z[0][1]][a] == 0):
+                boards[z[0][1]][a] = 1
+                pts+=1
+            elif(boards[z[1][1]][a] == 0):
+                boards[z[1][1]][a] = 1
+                pts+=1
+            elif(boards[z[2][1]][a] == 0):
+                boards[z[2][1]][a] = 1
+                pts+=1
+            elif(boards[z[3][1]][a] == 0):
+                boards[z[3][1]][a] = 1
+                pts+=1
+            else:
+                x = False
+
+            for u in range(4): 
+                it = 0
+                it_o = 0
+                if(boards[u][0] == 1 & boards[u][1] == 1 & boards[u][2] == 1 & 
+                   boards[u][3] == 1 & boards[u][4] == 1 & boards[u][5] == 1):
+                    if (u == 3):
+                        it = 2
+                        it_o = 0
+                    elif (u == 0):
+                        it = 1
+                        it_o = 3
+                    else:
+                        it = u -1
+                        it_o = u+1
+                    pts += 6
+                    temp_pts = sum(boards[it]) + sum(boards[it_o])
+                    pts+= temp_pts
+                    for c in range(6):
+                        boards[u][c] = 0
+                        boards[it][c] = 0
+                        boards[it_o][c] = 0
+    return pts
+
 #creates a list of averages for each method, plots as a histogram and performs a t-test
 def getResults(n):
     tot = 0
     num = n
     avgFirst = []
     avgSum = []
+    avgTotSum = []
     totTwo = 0
+    totThree = 0
     for y in range(num):
         tot = 0
         totTwo = 0
+        totThree = 0
         for x in range(num):
             tot += fillFirst()
             totTwo += fillSum()
+            totThree += fillSum()
         avgSum.append(totTwo/num)
         avgFirst.append(tot/num)
+        avgTotSum.append(totThree/num)
     bins = numpy.linspace(20, 60, 100)
     pyplot.hist(avgFirst, bins, alpha=0.5, label='first')
     pyplot.hist(avgSum, bins, alpha=0.5, label='sum')
+    pyplot.hist(avgTotSum, bins, alpha=0.5, label='adj-sum')
     pyplot.legend(loc='upper right')
     pyplot.show()
     t, p = ttest_ind(avgFirst, avgSum, equal_var=False)
